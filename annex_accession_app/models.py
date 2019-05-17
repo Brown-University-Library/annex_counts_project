67b384a202ff8d9a664fc0ac46377d2514eaa684
birkin <birkin.diana@gmail.com>
2019-05-17 11:21:12 -0400

    adds model.

--- annex_accession_app/models.py
+++ annex_accession_app/models.py
@@ -7,3 +7,13 @@ from django.db import models
 from django.http import HttpResponseRedirect
 
 log = logging.getLogger(__name__)
+
+
+class Counter(models.Model):
+    create_datetime = models.DateTimeField(auto_now_add=True, null=True)
+    date_key = models.DateField(unique=True)
+    hay_accessions = models.IntegerField()
+    hay_refiles = models.IntegerField()
+    non_hay_accessions = models.IntegerField()
+    non_hay_refiles = models.IntegerField()
+    notes = models.TextField(null=True)
