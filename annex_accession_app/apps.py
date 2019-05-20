dfd5140d1af5fc2f320d4ac5323abc6be16b4544
birkin <birkin.diana@gmail.com>
2019-05-20 10:09:14 -0400

    starts find-files-after-given-date code.

--- annex_accession_app/apps.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.apps import AppConfig
-
-
-class AppXConfig(AppConfig):
-    name = 'annex_accession_app'
---
+++ annex_accession_app/lib/updater.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import datetime, logging, os, pathlib, pprint, sys
+import django
+
+logging.basicConfig(
+    # filename=LOG_PATH,
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s', datefmt='%d/%b/%Y %H:%M:%S' )
+log = logging.getLogger(__name__)
+log.info( 'log started' )
+
+
+SOURCE_DIR_PATH = os.environ['ANX_ACC__SOURCE_DIR_PATH']
+
+
+def manage_update():
+    """ Manages processing.
+        Called by runner(); triggered by cron job. """
+    yesterday = datetime.date.today() - datetime.timedelta( days=1 )
+    files_list = find_files( yesterday )
+    1/0
+
+
+def find_files( date ):
+    """ Returns full paths of files created on or after given date.
+        Called by runner(); triggered by cron job.
+        Reference: <https://stackoverflow.com/a/539024> """
+    entries = os.listdir( SOURCE_DIR_PATH )
+    log.debug( 'entries, ```%s```' % pprint.pformat(entries) )
+    2/0
+    entries = ( os.path.join(SOURCE_DIR_PATH, fn) for fn in os.listdir(SOURCE_DIR_PATH) )
+    log.debug( 'entries, ```%s```' % pprint.pformat(entries) )
+    entries = ((os.stat(path), path) for path in entries)
+
+    # leave only regular files, insert creation date
+    entries = ((stat[ST_CTIME], path)
+               for stat, path in entries if S_ISREG(stat[ST_MODE]))
+
+
+if __name__ == '__main__':
+    manage_update()
