32bd86f8561feab21a8c31cc3840aad1c62188ee
birkin <birkin.diana@gmail.com>
2019-06-25 12:35:45 -0400

    enables updating of existing counts.

--- annex_counts_app/lib/updater.py
+++ annex_counts_app/lib/updater.py
@@ -9,30 +9,6 @@ from annex_counts_app.models import Counter
 log = logging.getLogger(__name__)
 
 
-# def check_validity( request ) -> bool:
-#     """ Checks request.
-#         Called by: views.updater() """
-#     check_val = False
-#     required_keys = [ 'auth_key', 'date', 'hay_accessions', 'hay_refiles', 'non_hay_accessions', 'non_hay_refiles' ]
-#     try:
-#         if sorted( request.POST.keys() ) == required_keys:
-#             log.debug( 'keys present' )
-#             values_check = 'init'
-#             for key in required_keys:
-#                 if len( request.POST[key] ) == 0:
-#                     values_check = 'fail'
-#                     break
-#             if values_check == 'init':
-#                 log.debug( 'keys have values' )
-#                 if request.POST['auth_key'] == settings_app.API_AUTH_KEY:
-#                     log.debug( 'auth_key good' )
-#                     check_val = True
-#     except Exception as e:
-#         log.error( f'validation failed, exception, ```{e}```' )
-#     log.debug( f'check_val, `{check_val}`' )
-#     return check_val
-
-
 class Validator:
 
     def __init__( self ):
@@ -99,16 +75,37 @@ def update_db( data: dict ) -> None:
         Called by views.updater() """
     try:
         record = Counter.objects.get( date_key=data['date'] )
-        log.debug( 'record already exists; not created or updated' )
+        log.debug( 'record already exists; will be updated' )
     except Exception as e:
-        log.debug( f"record for date ```{data['date']}``` not found, message is ```{e}```; will create counter-record" )
-        record = Counter(
-            date_key = data['date'],
-            hay_accessions = data['hay_accessions'],
-            hay_refiles = data['hay_refiles'],
-            non_hay_accessions = data['non_hay_accessions'],
-            non_hay_refiles = data['non_hay_refiles'],
-        )
-        record.save()
-        log.debug( f'record created, ```{pprint.pformat(record.__dict__)}```' )
+        log.debug( f"record for date ```{data['date']}``` not found, message is ```{e}```; will create record" )
+        record = Counter()
+    if record.date_key:  # means it already existed and was populated
+        record.notes = f'{record.notes}\n---\nupdated existing counts with new data: ```{pprint.pformat(data)}```'
+    else:
+        record.date_key = data['date']
+    record.hay_accessions += data['hay_accessions']
+    record.hay_refiles += data['hay_refiles']
+    record.non_hay_accessions += data['non_hay_accessions']
+    record.non_hay_refiles += data['non_hay_refiles']
+    record.save()
     return
+
+
+# def update_db( data: dict ) -> None:
+#     """ Updates data for given date.
+#         Called by views.updater() """
+#     try:
+#         record = Counter.objects.get( date_key=data['date'] )
+#         log.debug( 'record already exists; not created or updated' )
+#     except Exception as e:
+#         log.debug( f"record for date ```{data['date']}``` not found, message is ```{e}```; will create counter-record" )
+#         record = Counter(
+#             date_key = data['date'],
+#             hay_accessions = data['hay_accessions'],
+#             hay_refiles = data['hay_refiles'],
+#             non_hay_accessions = data['non_hay_accessions'],
+#             non_hay_refiles = data['non_hay_refiles'],
+#         )
+#         record.save()
+#         log.debug( f'record created, ```{pprint.pformat(record.__dict__)}```' )
+#     return
